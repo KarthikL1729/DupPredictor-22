b8f1014e3798e10e22a13964f26f0897a01fa8c5
KarthikL1729 <karthiklaksh1729@gmail.com>
2022-12-02 12:09:57 +0530

    Modified parsing for id

--- parse.py
+++ parse.py
@@ -13,7 +13,7 @@ def parse(filename):
 
     return data
 
-dat = parse('QueryResultsSU.csv')
+dat = parse('QueryResultsSU_new.csv')
 fields = dat[0]
 dat = dat[1:300]
 
@@ -27,79 +27,88 @@ with open('parsed_SU.csv', 'w') as f:
     writer.writerow(fields)
     for i in dat:
 
-        i[0] = h.handle(i[0])                                                           # Converting html to text
-        i[0] = list(tokenize(i[0], lowercase=True, deacc=True, errors='ignore'))        # Tokenizing
-        i[0] = [word for word in i[0] if word not in stopwords] # Removing stopwords
-        i[0] = [PorterStemmer().stem(word) for word in i[0]]                       # Stemming                        
+        i[1] = h.handle(i[1])                                                           # Converting html to text
+        i[1] = list(tokenize(i[1], lowercase=True, deacc=True, errors='ignore'))        # Tokenizing
+        i[1] = [word for word in i[1] if word not in stopwords] # Removing stopwords
+        i[1] = [PorterStemmer().stem(word) for word in i[1]]                       # Stemming                        
 
-        i[1].replace('<', ' ')                                                          # Replacing the characters and cleaning tags 
-        i[1].replace('>', ' ')
-        i[1].replace('-', ' ')
+        i[3].replace('<', ' ')                                                          # Replacing the characters and cleaning tags 
+        i[3].replace('>', ' ')
+        i[3].replace('-', ' ')
 
-        i[1] = list(tokenize(i[1]))
+        i[3] = list(tokenize(i[3]))
 
         i[2] = h.handle(i[2])
         i[2] = list(tokenize(i[2], lowercase=True, deacc=True, errors='ignore'))
         i[2] = [word for word in i[2] if word not in stopwords]
         i[2] = [PorterStemmer().stem(word) for word in i[2]] 
         
-        writer.writerow(i)
-
-dat_dup = parse('QueryResults_dup.csv')
-fields = dat_dup[0]
-dat_dup = dat_dup[1:80]
-
-with open('parsed_dup.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(fields)
-    for i in dat_dup:
-
-        i[3] = h.handle(i[3])                                                           # Converting html to text
-        i[3] = list(tokenize(i[3], lowercase=True, deacc=True, errors='ignore'))        # Tokenizing
-        i[3] = [word for word in i[3] if word not in stopwords] # Removing stopwords
-        i[3] = [PorterStemmer().stem(word) for word in i[3]]                       # Stemming                        
-
-        i[5].replace('<', ' ')                                                          # Replacing the characters and cleaning tags 
-        i[5].replace('>', ' ')
-        i[5].replace('-', ' ')
-
-        i[5] = list(tokenize(i[5]))
-
-        i[4] = h.handle(i[4])
-        i[4] = list(tokenize(i[4], lowercase=True, deacc=True, errors='ignore'))
-        i[4] = [word for word in i[4] if word not in stopwords]
-        i[4] = [PorterStemmer().stem(word) for word in i[4]] 
-
         j = list()
+        j.append(i[0])
+        j.append(i[1])
+        j.append(i[2])
         j.append(i[3])
-        j.append(i[5])
-        j.append(i[4])
 
         writer.writerow(j)
+
+dat_dup = parse('QueryResults_dup_new.csv')
+fields = dat_dup[0]
+dat_dup = dat_dup[1:80]
 
 with open('parsed_SU.csv', 'a') as f:
     writer = csv.writer(f)
     for i in dat_dup:
 
-        i[0] = h.handle(i[0])                                                           # Converting html to text
-        i[0] = list(tokenize(i[0], lowercase=True, deacc=True, errors='ignore'))        # Tokenizing
-        i[0] = [word for word in i[0] if word not in stopwords] # Removing stopwords
-        i[0] = [PorterStemmer().stem(word) for word in i[0]]                       # Stemming                        
+        i[1] = h.handle(i[1])                                                           # Converting html to text
+        i[1] = list(tokenize(i[1], lowercase=True, deacc=True, errors='ignore'))        # Tokenizing
+        i[1] = [word for word in i[1] if word not in stopwords] # Removing stopwords
+        i[1] = [PorterStemmer().stem(word) for word in i[1]]                       # Stemming                        
 
-        i[2].replace('<', ' ')                                                          # Replacing the characters and cleaning tags 
-        i[2].replace('>', ' ')
-        i[2].replace('-', ' ')
+        i[3].replace('<', ' ')                                                          # Replacing the characters and cleaning tags 
+        i[3].replace('>', ' ')
+        i[3].replace('-', ' ')
 
-        i[2] = list(tokenize(i[2]))
+        i[3] = list(tokenize(i[3]))
 
-        i[1] = h.handle(i[1])
-        i[1] = list(tokenize(i[1], lowercase=True, deacc=True, errors='ignore'))
-        i[1] = [word for word in i[1] if word not in stopwords]
-        i[1] = [PorterStemmer().stem(word) for word in i[1]] 
-
+        i[2] = h.handle(i[2])
+        i[2] = list(tokenize(i[2], lowercase=True, deacc=True, errors='ignore'))
+        i[2] = [word for word in i[2] if word not in stopwords]
+        i[2] = [PorterStemmer().stem(word) for word in i[2]] 
+        
         j = list()
         j.append(i[0])
-        j.append(i[2])
         j.append(i[1])
+        j.append(i[2])
+        j.append(i[3])
         
+        writer.writerow(j)
+
+
+with open('parsed_dup.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(fields[4:8])
+    for i in dat_dup:
+
+        i[5] = h.handle(i[5])                                                           # Converting html to text
+        i[5] = list(tokenize(i[5], lowercase=True, deacc=True, errors='ignore'))        # Tokenizing
+        i[5] = [word for word in i[5] if word not in stopwords] # Removing stopwords
+        i[5] = [PorterStemmer().stem(word) for word in i[5]]                       # Stemming                        
+
+        i[7].replace('<', ' ')                                                          # Replacing the characters and cleaning tags 
+        i[7].replace('>', ' ')
+        i[7].replace('-', ' ')
+
+        i[7] = list(tokenize(i[7]))
+
+        i[6] = h.handle(i[6])
+        i[6] = list(tokenize(i[6], lowercase=True, deacc=True, errors='ignore'))
+        i[6] = [word for word in i[6] if word not in stopwords]
+        i[6] = [PorterStemmer().stem(word) for word in i[6]] 
+
+        j = list()
+        j.append(i[4])
+        j.append(i[5])
+        j.append(i[6])
+        j.append(i[7])
+
         writer.writerow(j)
